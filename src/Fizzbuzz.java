31d84458c4037b3ea42b29d06f9b8210207b5edd
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-03-04 14:07:45 +0000

    added test case for zero

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -2,6 +2,7 @@ public class Fizzbuzz {
 	
 	public String play(int num) {
 		
+		if (num == 0) throw new IllegalArgumentException("Number must not be zero");
 		if (num % 15 == 0) return "FizzBuzz";
 		if (num % 5 == 0) return "Buzz";
 		if (num % 3 == 0) return "Fizz";
--- src/FizzbuzzTest.java
+++ src/FizzbuzzTest.java
@@ -1,35 +1,48 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FizzbuzzTest extends Fizzbuzz {
 	
+	public Fizzbuzz fizzbuzz;
+	@BeforeEach
+	public void before() {
+		fizzbuzz = new Fizzbuzz();
+	}
+	
 	@DisplayName("play fizzbuzz with the number 1")
 	@Test
 	public void testNumber() {
-		String fizzbuzz = new Fizzbuzz().play(1);
-		Assertions.assertEquals(fizzbuzz, "1");
+		String output = fizzbuzz.play(1);
+		Assertions.assertEquals(output, "1");
 	}
 	
 	@DisplayName("play fizzbuzz with the number 3")
 	@Test
 	public void testFizz() {
-		String fizzbuzz = new Fizzbuzz().play(3);
-		Assertions.assertEquals(fizzbuzz, "Fizz");
+		String output = fizzbuzz.play(3);
+		Assertions.assertEquals(output, "Fizz");
 	}
 	
 	@DisplayName("play fizzbuzz with the number 5")
 	@Test
 	public void testBuzz() {
-		String fizzbuzz = new Fizzbuzz().play(5);
-		Assertions.assertEquals(fizzbuzz, "Buzz");
+		String output = fizzbuzz.play(5);
+		Assertions.assertEquals(output, "Buzz");
 	}
 	
 	@DisplayName("play fizzbuzz with the number 15")
 	@Test
 	public void testFizzBuzz() {
-		String fizzbuzz = new Fizzbuzz().play(15);
-		Assertions.assertEquals(fizzbuzz, "FizzBuzz");
+		String output = fizzbuzz.play(15);
+		Assertions.assertEquals(output, "FizzBuzz");
+	}
+	
+	@DisplayName("dont play fizzbuzz with the number 0")
+	@Test
+	public void testzero() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> fizzbuzz.play(0));
 	}
 	
 //	@Test
